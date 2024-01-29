f72018e28fa21ba9533790288b87f4651f75df79
Han Zeng <hanyu.tseng@yahooinc.com>
2024-01-29 15:17:07 +0800

    feat: 1.0.5 with @luxass/strip-json-comments

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const { strip } = require('@luxass/strip-json-comments')
+
+const json = `{
+    // this is a comment
+    "foo": /* this is also a comment */ "bar"
+  }`;
+  
 function main() {
+    const stripReult = JSON.parse(strip(json)); // { foo: "bar" }
     console.log('==== This is obsoleted package log ====')
+    console.log('==== stripReult ====\n', stripReult)
+    console.log('==== end ====')
 }
 
 module.exports = {
